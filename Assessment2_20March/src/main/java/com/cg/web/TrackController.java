bb3c77932f9ab51f5016271a452f973830f9cb14
Shubham Choudhary <shubham1212choudhary@gmail.com>
2026-03-20 14:26:30 +0530

    assessment 2 uploaded successfully

--- Assessment2_20March/src/main/java/com/cg/web/TrackController.java
+++ Assessment2_20March/src/main/java/com/cg/web/TrackController.java
@@ -1,6 +1,7 @@
 package com.cg.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -41,7 +42,11 @@ public class TrackController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getTrack(@PathVariable Long id) {
-    	
-        return repo.findById(id).<ResponseEntity<Object>>map(track -> ResponseEntity.ok(track)).orElseGet(() -> ResponseEntity.notFound().build());
+    	Optional<Track> trackOpt = repo.findById(id);
+    	if(trackOpt.isPresent()) {
+    		return ResponseEntity.ok(trackOpt.get());
+    	} else {
+    		return ResponseEntity.notFound().build();
+    	}
     }
 }
